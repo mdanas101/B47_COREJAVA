032717d8fe6030aa259f1e66aeaa2e8638fd2889
sheik samsudeen <sheik samsudeen@DESKTOP-NRAA320>
2023-03-16 16:40:59 +0530

    FirstCommit

---
+++ c2tc.vvit/src/com/tns/b47/CarClass.java
@@ -0,0 +1,22 @@
+package com.tns.b47;
+/*
+ * Program to demonstrate class and objects
+ */
+public class CarClass {
+	// Data members for the class
+	int model;
+	String color;
+	int reg;
+	// member functions for class
+	void forward() {
+		System.out.println("Car is moving in forward direction");
+	}
+		
+    void backward() {
+    	System.out.println("Car is moving in backward direction");
+    }
+
+    void stop() {
+    	System.out.println("Car is not moving");
+}
+}
---
+++ c2tc.vvit/src/com/tns/b47/Executor.java
@@ -0,0 +1,13 @@
+package com.tns.b47;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	CarClass ob = new CarClass ();//creation of object
+	ob.backward();
+	ob.forward();
+	ob.stop();
+	}
+
+}
