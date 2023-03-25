dfc5ba6e12940066c291a7ca8d6f3148f2bd01d5
sheik samsudeen <sheik samsudeen@DESKTOP-NRAA320>
2023-03-25 17:27:37 +0530

    Sixth Commit

---
+++ c2tc.vvit/src/com/tns/overloading/OverloadingDemo.java
@@ -0,0 +1,12 @@
+package com.tns.overloading;
+
+public class OverloadingDemo {
+	public static void main(String[] args) {
+		Point ob=new Point();
+		System.out.println(ob);
+		Point ob1=new Point(14.5f);
+		System.out.println(ob1);
+		
+	}
+
+}
